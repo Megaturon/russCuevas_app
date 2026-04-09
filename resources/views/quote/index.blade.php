dab2f562e36fa3053df7d2d77bf98e8769376959
francisshere <francisadriangapol18@gmail.com>
2026-04-09 20:02:56 +0800

    finally FIXED css of getAQuote

--- resources/views/quote/index.blade.php
+++ resources/views/quote/index.blade.php
@@ -3,9 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{ asset('style2.css') }}">
     <title>Get a Quote - Russ Cuevas Artelier</title>
-    
+
     <!-- React, ReactDOM, and Babel Scripts -->
     <script src="https://unpkg.com/react@17/umd/react.development.js"></script>
     <script src="https://unpkg.com/react-dom@17/umd/react-dom.development.js"></script>
@@ -13,6 +12,8 @@
     
     <!-- Tailwind CSS -->
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+
+    <link rel="stylesheet" href="{{ asset('style2.css') }}">
 </head>
 <body>
     <header>
