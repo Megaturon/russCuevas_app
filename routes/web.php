0f05194721c30158a384d041f7ac99f280e93383
francisshere <francisadriangapol18@gmail.com>
2026-05-10 23:03:37 +0800

    fixed the redirection for /

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\QuoteController;
 use App\Http\Controllers\AuthController;
 
-Route::redirect('/', '/main');
+Route::get('/', function () {
+    return redirect('/main');
+});
 Route::redirect('/login-page', '/login');
 Route::redirect('/signup-page', '/signup');
 Route::redirect('/forgot-password', '/forget-password');
