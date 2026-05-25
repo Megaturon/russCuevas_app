a7d28ad73ef229c76e168e44f55d111f5de1af7e
Renzo Cuevas <cuevas_renzo@plpasig.edu.ph>
2026-05-25 19:35:20 +0800

    Merged with feat/clien-ui-fix

--- resources/views/login-page.blade.php
+++ resources/views/login-page.blade.php
@@ -78,19 +78,6 @@
   </div>
 
   <script>
-    function togglePasswordVisibility(inputId, icon) {
-        const input = document.getElementById(inputId);
-        if (input.type === 'password') {
-            input.type = 'text';
-            icon.classList.remove('fa-eye');
-            icon.classList.add('fa-eye-slash');
-        } else {
-            input.type = 'password';
-            icon.classList.remove('fa-eye-slash');
-            icon.classList.add('fa-eye');
-        }
-    }
-
     document.addEventListener('DOMContentLoaded', function() {
         const items = document.querySelectorAll('.carousel-item');
         let current = 0;
