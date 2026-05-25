032ef56a49aaa23924d0fa5965146873570c9325
francisshere <francisadriangapol18@gmail.com>
2026-05-25 17:05:54 +0800

    brought back the our-story page and fixed the messed up footer

--- resources/views/our-story.blade.php
+++ resources/views/our-story.blade.php
@@ -8,7 +8,7 @@
   @vite(['resources/css/styles2.css', 'resources/css/our-story.css'])
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
 <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
-<link rel="stylesheet" href="{{ asset('style2.css') }}">
+
 </head>
 <body>
 
