01ef052e55625ace28f2b713d6b860288d601160
francisshere <francisadriangapol18@gmail.com>
2026-05-25 17:35:57 +0800

    changed the  bg images on the login and signup pages to avoid copyright

--- resources/views/login-page.blade.php
+++ resources/views/login-page.blade.php
@@ -20,9 +20,9 @@
 
   <div class="auth-centered-wrapper">
     <div class="auth-bg-carousel">
-      <div class="carousel-item active" style="background-image: url('/img/slide1.jpg');"></div>
-      <div class="carousel-item" style="background-image: url('/img/slide2.webp');"></div>
-      <div class="carousel-item" style="background-image: url('/img/slide3.jpg');"></div>
+      <div class="carousel-item active" style="background-image: url('/img/4.jpg');"></div>
+      <div class="carousel-item" style="background-image: url('/img/5.jpg');"></div>
+      <div class="carousel-item" style="background-image: url('/img/model3.jpg');"></div>
       <div class="auth-overlay"></div>
     </div>
 
